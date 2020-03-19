7d92dc59b8e8b82dc6f0008ec42479185cdfd17e
setiyawan12 <yayangsetiyawa0@gmail.com>
2020-03-19 18:51:47 +0700

    tugas

---
+++ src/Latihan/Mhs.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Choco Calon Presiden RI
+ */
+public class Mhs {
+     public static void main(String[] args) throws ClassNotFoundException {
+        FileOutputStream fos;
+        ObjectOutputStream oos;
+        FileInputStream fis;
+        ObjectInputStream ois;
+        
+        try {
+            Scanner s = new Scanner(System.in).useDelimiter("\n");
+            System.out.print("Masukan NIM : ");
+                int nim = s.nextInt();
+            System.out.print("Masukan Nama : ");
+            //Pattern p = Pattern.compile("[a-zA-Z ]+")
+            String nama = s.next();
+            System.out.println("=>"+nama);
+            Data data = new Data (nim,nama);
+             File f = new File("data.ser");
+                if (f.exists()) {
+                   // f.delete();
+            }
+                
+          fos = new FileOutputStream(f);
+          oos = new ObjectOutputStream(fos);
+             oos.writeObject(data);
+             oos.close();
+             fos.close();
+             
+           fis = new FileInputStream(f);
+           ois = new ObjectInputStream(fis);
+           Object obj = ois.readObject();    
+           Data input = (Data) obj;
+           ois.close();
+           fis.close();
+            System.out.println(input.getNama());
+        } catch (Exception e) {
+            System.err.println(""+e.getMessage());
+        }
+    }
+    static class Data implements Serializable{
+    
+    int nim;
+    String nama;
+    public  Data(int _nim, String _nama){
+            nim = _nim;
+            nama = _nama;
+        
+        }
+
+        public int getNim() {
+            return nim;
+        }
+
+        public String getNama() {
+            return nama;
+        }
+
+        @Override
+        public String toString() {
+            return "Data{" + "nim=" + nim +"\n"+
+                    ", nama=" + nama + '}';
+        }
+    
+    }
+}
---
+++ src/serialisasi/ProductItem.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serialisasi;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Choco Calon Presiden RI
+ */
+
+public class ProductItem  implements Serializable{
+    String nama;
+    double harga;
+    
+    public ProductItem(String name, double price){
+        this.nama = name;
+        this.harga = price;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductItem{" + "nama=" + nama + ", harga=" + harga + '}';
+    }
+}
+
+
